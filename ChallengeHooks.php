a6d3455316d12c5d3b3ee0e616dc4ab9482e7c18
Jack Phoenix <jack@countervandalism.net>
2016-09-02 19:52:34 +0300

    Version 1.1: extension registration support, PHP entry point removed; MW 1.25+ is required
    Change-Id: I1ac2d8c08ad0b9ec49e7cf4e9a9ffabc8b9d20ec

---
+++ ChallengeHooks.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @file
+ */
+class ChallengeHooks {
+
+	/**
+	 * Adds the three new required database tables into the database when the
+	 * user runs /maintenance/update.php (the core database updater script).
+	 *
+	 * @param DatabaseUpdater $updater
+	 * @return bool
+	 */
+	public static function onLoadExtensionSchemaUpdates( $updater ) {
+		$dir = __DIR__;
+
+		$dbType = $updater->getDB()->getType();
+
+		$filename = 'challenge.sql';
+		// For non-MySQL/MariaDB/SQLite DBMSes, use the appropriately named file
+		/*
+		if ( !in_array( $dbType, array( 'mysql', 'sqlite' ) ) ) {
+			$filename = "challenge.{$dbType}.sql";
+		}
+		*/
+
+		$updater->addExtensionUpdate( array( 'addTable', 'challenge', "{$dir}/{$filename}", true ) );
+		$updater->addExtensionUpdate( array( 'addTable', 'challenge_rate', "{$dir}/{$filename}", true ) );
+		$updater->addExtensionUpdate( array( 'addTable', 'challenge_user_record', "{$dir}/{$filename}", true ) );
+
+		return true;
+	}
+
+}
