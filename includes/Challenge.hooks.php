aebe050195ae3632406a93a2c3e5878cc25cbdda
Jack Phoenix <ashley@uncyclomedia.co>
2020-01-03 23:15:30 +0200

    Add a "challenge this user" link to social profile pages
    Bug: T152930
    Change-Id: Idb1efdb2f65a6993fe7a5b237fa79146fd84d8dd

--- includes/Challenge.hooks.php
+++ includes/Challenge.hooks.php
@@ -28,4 +28,24 @@ class ChallengeHooks {
 		$updater->addExtensionTable( 'challenge_user_record', "{$dir}/{$filename}" );
 	}
 
+	/**
+	 * Adds a "challenge this user" link to the social user profile pages
+	 *
+	 * @param UserProfilePage $upp
+	 * @param array &$profileLinks Array of existing profile links
+	 */
+	public static function onUserProfileGetProfileHeaderLinks( $upp, &$profileLinks ) {
+		// $upp->getContext()->getUser() refers to the currently _viewing_ user, not
+		// to the user whose profile is being viewed
+		// Show this link only to registered users who are *not* viewing their own profile
+		$ctx = $upp->getContext();
+		if ( $ctx->getUser()->isLoggedIn() && !$upp->isOwner() ) {
+			$challengeUser = SpecialPage::getTitleFor( 'ChallengeUser', $upp->user_name );
+			$profileLinks['challenge-user'] =
+				'<a href="' . htmlspecialchars( $challengeUser->getFullURL(), ENT_QUOTES ) . '" rel="nofollow">' .
+					$ctx->msg( 'challenge-this-user' )->escaped() .
+				'</a>';
+		}
+	}
+
 }
