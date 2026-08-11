acbdb9439f801b627e2fb6507933a45e9efc929c
peterxy12 <peterxyjie@gmail.com>
2026-08-11 19:00:36 +0800

    Challenge: Update Chinese translations
    Bug: T399285
    Change-Id: Ia8a250f9044211a0b152d9d7d794939163b8bb15

--- includes/Challenge.alias.php
+++ includes/Challenge.alias.php
@@ -25,3 +25,35 @@ $specialPageAliases['fi'] = [
 	'ChallengeUser' => [ 'Haasta_käyttäjä' ],
 	'ChallengeView' => [ 'Tarkastele_haastetta' ],
 ];
+
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'ChallengeAction' => [ 'ChallengeAction' ],
+	'ChallengeHistory' => [ 'ChallengeHistory' ],
+	'ChallengeStandings' => [ 'ChallengeStandings' ],
+	'ChallengeUser' => [ 'ChallengeUser' ],
+	'ChallengeView' => [ 'ChallengeView' ],
+];
+
+/** Simplified Chinese (中文（简体）) */
+$specialPageAliases['zh-hans'] = [
+	'ChallengeAction' => [ '挑战操作' ],
+	'ChallengeHistory' => [ '挑战历史' ],
+	'ChallengeStandings' => [ '挑战排名' ],
+	'ChallengeUser' => [ '挑战用户' ],
+	'ChallengeView' => [ '查看挑战' ],
+];
+
+/** Traditional Chinese (中文（繁體）) */
+$specialPageAliases['zh-hant'] = [
+	'ChallengeAction' => [ '挑戰操作' ],
+	'ChallengeHistory' => [ '挑戰歷史' ],
+	'ChallengeStandings' => [ '挑戰排名' ],
+	'ChallengeUser' => [ '挑戰使用者' ],
+	'ChallengeView' => [ '檢視挑戰' ],
+];
+
+/** Chinese (Hong Kong) (中文（香港）) */
+$specialPageAliases['zh-hk'] = [
+	'ChallengeUser' => [ '挑戰用戶' ],
+];
